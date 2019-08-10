3174ba7a69cf92efc8928818cfb7907084303b1d
LiuXing <liu__xing@163.com>
2019-08-10 13:31:43 +0800

    添加工程

---
+++ IDEA_Project/Dome/src/Hello.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+
+public class Hello {
+   static class a {
+        public void say() {
+            System.out.println("hahahah");
+
+        }
+    }
+    public static void main(String[] args) {
+
+    a hello = new a();
+
+    hello.say();
+
+
+
+    }
+}
